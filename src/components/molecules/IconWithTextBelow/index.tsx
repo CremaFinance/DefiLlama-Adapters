f3a4d3ee1294d0289a09203fba33a54ff71786ad
Alexandru.Stefan <alex.steefan@gmail.com>
2021-10-01 16:37:46 +0200

    refactor!: extract iconWithTextBelow as a molecule

---
+++ src/components/molecules/IconWithTextBelow/index.tsx
@@ -0,0 +1,44 @@
+import { Heading, Image, Text, Square } from "@chakra-ui/react";
+import * as React from "react";
+
+type IconWithTextBelowProps = {
+  title: string;
+  subtitle?: string;
+  icon: string;
+};
+
+const IconWithTextBelow = (props: IconWithTextBelowProps) => (
+  <Square
+    display="flex"
+    flexDirection="column"
+    flex="1"
+    marginLeft="5"
+    marginRight="5"
+    marginBottom="5"
+    justifyContent="flex-start"
+    maxWidth="320"
+    minWidth="320"
+  >
+    <Image
+      src={props.icon}
+      maxW="sm"
+      objectFit="contain"
+      alt={`${props.title} Logo`}
+    />
+    <Heading
+      size="md"
+      color="white"
+      marginBottom="5"
+      marginTop="5"
+      wordBreak="keep-all"
+      textAlign="center"
+    >
+      {props.title}
+    </Heading>
+    <Text color="white" textAlign="center">
+      {props.subtitle}
+    </Text>
+  </Square>
+);
+
+export default IconWithTextBelow;
