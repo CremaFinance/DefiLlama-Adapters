7ac4df02b1be258cc8a64d01c9698bb1dd5f9204
SirDespin <sirdespin@gmail.com>
2021-09-30 23:33:25 -0300

    feat(accessibility): :wheelchair: added i18n
    I added i18n to the project. Also, the strings for 'why' section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,20 @@
 import { Box } from "@chakra-ui/react";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 import LogoStripSection from "components/index/LogoStripSection";
 import HeroSection from "components/index/HeroSection";
 import TestemonialSection from "components/index/TestimonialSection";
+interface StaticProps {
+  locale: string;
+}
+
+export async function getStaticProps({ locale }: StaticProps) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common", "index"])),
+    },
+  };
+}
 
 const Home = () => {
   return (
