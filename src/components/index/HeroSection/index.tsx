6e4494ae50ffa11bb918d0d091d48eec8a631828
Alexandru.Stefan <alex.steefan@gmail.com>
2021-10-16 23:19:20 +0200

    refactor: update resources and margins based on design

--- src/components/index/HeroSection/index.tsx
+++ src/components/index/HeroSection/index.tsx
@@ -1,4 +1,4 @@
-import { HStack, Button, Image, Box, Heading } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Image, Text } from "@chakra-ui/react";
 import { useTranslation } from "next-export-i18n";
 
 import colors from "styles/customTheme/colors";
@@ -11,132 +11,102 @@ const HeroSection = () => {
       py="72px"
       pb={[12, 12, 8]}
       bg={colors.greenLight}
+      aria-label="hero-section"
       position="relative"
       height={["auto", "auto", "100vh"]}
-      minHeight="650px"
+      minHeight="640px"
     >
       <Box
-        pt={[5, 5, "10vh"]}
-        pl={[0, 0, 10]}
+        pt={[4, 4, "10vh"]}
+        pl={[0, 0, 160]}
         position="relative"
         zIndex={10}
         textAlign={["center", "center", "left"]}
       >
-        <Box mb={[2, 2, 4]}>
+        <Box mb={4} maxWidth={[320, 400]}>
           <Heading color={colors.greenVibrant} fontWeight="bold">
             {t("indexPage.hero-section-title")}
           </Heading>
 
-          <Heading
-            fontWeight="bold"
-            maxWidth={300}
-            margin={["0 auto", "0 auto", "0"]}
-          >
+          <Heading fontWeight="bold" margin={["0 auto", "0 auto", "0"]}>
             {t("indexPage.hero-section-subtitle")}
           </Heading>
         </Box>
 
-        <Box maxWidth={300} margin={["0 auto", "0 auto", "0"]} mb={[2, 2, 4]}>
-          <Box>{t("indexPage.hero-section-desc")}</Box>
-        </Box>
+        <Text
+          maxWidth={300}
+          margin={["0 auto", "0 auto", "0"]}
+          mb={[2, 2, 4]}
+          fontSize="22"
+          color={colors.black}
+        >
+          {t("indexPage.hero-section-desc")}
+        </Text>
 
-        <Box mb={[2, 2, 4]}>
-          <Button bg={colors.green} colorScheme={colors.green} rounded="md">
-            {t("indexPage.hero-section-button")}
-          </Button>
-        </Box>
+        <Button
+          bg={colors.green}
+          colorScheme={colors.green}
+          rounded="md"
+          mb={[2, 2, 4]}
+        >
+          {t("indexPage.hero-section-button")}
+        </Button>
 
-        <Box fontSize="sm" color="gray.500">
+        <Text fontSize="14" color="gray.500">
           6.21% APY
-        </Box>
+        </Text>
       </Box>
 
-      {/* CLOUD AND LEAVES IMAGE */}
-      <Box display={["none", "none", "block"]}>
+      <Flex width="100vw" justifyContent="flex-end">
         <Image
-          src="./cloud-leaves.png"
-          width={[0, 0, 350, "420px"]}
+          src="./ilustrations/clouds-and-leaves.svg"
+          width={[0, 0, "32vw"]}
           alt="Clouds and Leaves"
           position="absolute"
-          top="0"
-          right={0}
+          top={-8}
           zIndex={3}
         />
-      </Box>
 
-      {/* BEACH PARTY IMAGE */}
-      <Box
-        position={["relative", "relative", "absolute"]}
-        top={[0, 0, "10vh", "10vh"]}
-        overflow="hidden"
-        textAlign="center"
-        right={0}
-        my={[4, 2, 0]}
-      >
         <Image
           display="inline-block"
-          position="relative"
-          mt={[0, 0, 24, 10]}
-          src="./beach-party.png"
+          position={["relative", "relative", "absolute"]}
+          top={[0, 0, "16vh"]}
+          overflow="hidden"
+          my={[4, 2, 0]}
+          mt={[0, 0, 8]}
+          src="./ilustrations/hero.svg"
           alt="Friends on beach"
-          width={["100%", "100%", 600, "800px"]}
+          width={["100%", "72vw"]}
+          zIndex={4}
         />
-      </Box>
+      </Flex>
 
-      {/* MOBILE LAYOUT */}
+      {/* ECOSYSTEM STATS */}
       <Box
-        fontSize="lg"
-        px={5}
-        display={["block", "block", "none"]}
-        textAlign="center"
+        pl={["16px", "16px", 160]}
+        position="absolute"
+        bottom={16}
+        display="flex"
+        flexDirection={["column", "row"]}
       >
         {[0, 1, 2].map((index) => {
           return (
-            <Box key={`mobile-layout-${index}`}>
-              <Box
-                fontWeight="bold"
-                display="inline-block"
-                color={colors.green}
-                fontSize={["xl", "2xl"]}
-                mr={2}
-              >
+            <Box
+              display="flex"
+              flexDirection="row"
+              mr="16"
+              key={`desktop-hstack-${index}`}
+            >
+              <Heading size="md" fontWeight="bold" color={colors.green} mr={1}>
                 {t(`indexPage.hero-section-stats.${index}.number`)}
-              </Box>
+              </Heading>
 
-              <Box
-                fontSize={["xl", "2xl"]}
-                fontWeight="bold"
-                display="inline-block"
-              >
+              <Heading size="md" fontWeight="bold">
                 {t(`indexPage.hero-section-stats.${index}.desc`)}
-              </Box>
+              </Heading>
             </Box>
           );
         })}
-      </Box>
-
-      {/* DESKTOP LAYOUT */}
-      <Box
-        pl={10}
-        position="absolute"
-        bottom={12}
-        display={["none", "none", "block"]}
-      >
-        <HStack spacing={8}>
-          {[0, 1, 2].map((index) => {
-            return (
-              <HStack fontSize="lg" key={`desktop-hstack-${index}`}>
-                <Heading size="md" fontWeight="bold" color={colors.green}>
-                  {t(`indexPage.hero-section-stats.${index}.number`)}
-                </Heading>
-
-                <Heading size="md" fontWeight="bold">
-                  {t(`indexPage.hero-section-stats.${index}.desc`)}
-                </Heading>
-              </HStack>
-            );
-          })}
-        </HStack>
       </Box>
     </Box>
   );
