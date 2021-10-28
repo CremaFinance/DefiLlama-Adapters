5369997bf452460d89c7a5ad5462b3ca92f47498
SirDespin <sirdespin@gmail.com>
2021-10-28 19:52:56 -0300

    feat: :lipstick: added common variants

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,5 +1,42 @@
 import { Button as ChakraButton, ButtonProps } from "@chakra-ui/react";
 
-const Button = (props: ButtonProps) => <ChakraButton {...props} />;
+const Button = (props: ButtonProps) => {
+  const { variant, children, ...rest } = props;
+  switch (variant) {
+    case "solid":
+      return (
+        <ChakraButton
+          variant="solid"
+          size="sm"
+          rounded="md"
+          bg="green"
+          _hover={{ bg: "green800" }}
+          colorScheme="green"
+          color="white"
+          {...rest}
+        >
+          {children}
+        </ChakraButton>
+      );
+    case "outline":
+      return (
+        <ChakraButton
+          size="sm"
+          rounded="md"
+          variant="outline"
+          bg="white"
+          color="green"
+          _hover={{ color: "green800" }}
+          colorScheme="green"
+          borderRadius="8px"
+          {...rest}
+        >
+          {children}
+        </ChakraButton>
+      );
+    default:
+      return <ChakraButton {...props} />;
+  }
+};
 
 export default Button;
