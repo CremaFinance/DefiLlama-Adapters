5bf4799daa99e80f128264f96b0e9bca6f7f3a63
SirDespin <sirdespin@gmail.com>
2021-11-01 17:43:09 -0300

    feat: :bug: adding ref forwaring

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,42 +1,47 @@
 import { Button as ChakraButton, ButtonProps } from "@chakra-ui/react";
+import { forwardRef, LegacyRef } from "react";
 
-const Button = (props: ButtonProps) => {
-  const { variant, children, ...rest } = props;
-  switch (variant) {
-    case "solid":
-      return (
-        <ChakraButton
-          variant="solid"
-          size="sm"
-          rounded="md"
-          bg="green"
-          _hover={{ bg: "green800" }}
-          colorScheme="green"
-          color="white"
-          {...rest}
-        >
-          {children}
-        </ChakraButton>
-      );
-    case "outline":
-      return (
-        <ChakraButton
-          size="sm"
-          rounded="md"
-          variant="outline"
-          bg="white"
-          color="green"
-          _hover={{ color: "green800" }}
-          colorScheme="green"
-          borderRadius="8px"
-          {...rest}
-        >
-          {children}
-        </ChakraButton>
-      );
-    default:
-      return <ChakraButton {...props} />;
+const Button = forwardRef(
+  (props: ButtonProps, ref: LegacyRef<HTMLButtonElement> | undefined) => {
+    const { variant, children, ...rest } = props;
+    switch (variant) {
+      case "solid":
+        return (
+          <ChakraButton
+            variant="solid"
+            size="sm"
+            rounded="md"
+            bg="green"
+            _hover={{ bg: "green800" }}
+            colorScheme="green"
+            color="white"
+            ref={ref}
+            {...rest}
+          >
+            {children}
+          </ChakraButton>
+        );
+      case "outline":
+        return (
+          <ChakraButton
+            size="sm"
+            rounded="md"
+            variant="outline"
+            bg="white"
+            color="green"
+            _hover={{ color: "green800" }}
+            colorScheme="green"
+            borderRadius="8px"
+            ref={ref}
+            {...rest}
+          >
+            {children}
+          </ChakraButton>
+        );
+      default:
+        return <ChakraButton ref={ref} {...props} />;
+    }
   }
-};
+);
 
 export default Button;
