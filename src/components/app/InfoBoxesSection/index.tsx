c3670eef8ef1e3d55a964ad2c50cc004cc7a1e6b
Alexandru.Stefan <alex@ndru.dev>
2021-11-02 21:44:50 +0100

    feat: add behaviour for mobile res

--- src/components/app/InfoBoxesSection/index.tsx
+++ src/components/app/InfoBoxesSection/index.tsx
@@ -5,6 +5,7 @@ import { MdInfoOutline } from "react-icons/md";
 import MLink from "../../atoms/Link";
 import MText from "../../atoms/Text";
 import colors from "styles/customTheme/colors";
+import { numberToShortVersion } from "utils/number-to-short-version";
 import { numberWithCommas } from "utils/number-with-commas";
 import { secondsToDhms } from "utils/seconds-to-dmhs";
 
@@ -21,136 +22,211 @@ const InfoBoxesSection = () => {
   const validators = 411;
 
   return (
-    <Flex
-      aria-label="info-boxes-section"
-      px={{ base: 4, md: "11vw" }}
-      justifyContent="space-around"
-      flexWrap="wrap"
-    >
+    <>
       <Flex
-        bg={colors.white}
-        flexDirection="column"
-        justifyContent="space-between"
-        rounded="lg"
-        width="207px"
-        height="139px"
-        zIndex={5}
-        py={5}
-        px={6}
-        mt={8}
+        display={["none", "flex"]}
+        aria-label="info-boxes-section"
+        px={{ base: 4, md: "11vw" }}
+        justifyContent="space-around"
+        flexWrap="wrap"
       >
-        <MText type="text-md">{t("appPage.info-msol-sol-price")}</MText>
-        <MText type="heading-xsm">{mSOLvsSOLParity} SOL</MText>
-        <MText type="text-md">≈ ${solPrice * mSOLvsSOLParity}</MText>
+        <Flex
+          bg={colors.white}
+          flexDirection="column"
+          justifyContent="space-between"
+          rounded="lg"
+          width="207px"
+          height="139px"
+          zIndex={5}
+          py={5}
+          px={6}
+          mt={8}
+        >
+          <MText type="text-md">{t("appPage.info-msol-sol-price")}</MText>
+          <MText type="heading-xsm">{mSOLvsSOLParity} SOL</MText>
+          <MText type="text-md" pb={2}>
+            ≈ ${solPrice * mSOLvsSOLParity}
+          </MText>
+        </Flex>
+        <Flex
+          bg={colors.white}
+          flexDirection="column"
+          justifyContent="space-between"
+          rounded="lg"
+          width="207px"
+          height="139px"
+          zIndex={5}
+          py={5}
+          px={6}
+          mt={8}
+        >
+          <MText type="text-md">{t("appPage.info-total-sol-staked")}</MText>
+          <MText type="heading-xsm">{numberWithCommas(totalSOLStaked)}</MText>
+          <MText type="text-md" pb={2}>
+            ≈ ${numberWithCommas(solPrice * totalSOLStaked)}
+          </MText>
+        </Flex>
+        <Flex
+          bg={colors.white}
+          flexDirection="column"
+          justifyContent="space-between"
+          rounded="lg"
+          width="207px"
+          height="139px"
+          zIndex={5}
+          py={5}
+          pr={3}
+          pl={6}
+          mt={8}
+        >
+          <Flex justifyContent="space-between">
+            <MText type="text-md">{t("appPage.info-epoch")}</MText>
+            <IconButton
+              variant="link"
+              aria-label="Info epoch"
+              size="sm"
+              icon={<MdInfoOutline />}
+            />
+          </Flex>
+          <Flex alignItems="center" justifyContent="space-between">
+            <MText type="heading-xsm">{epochProgress}%</MText>
+            <Progress
+              value={epochProgress}
+              width="94px"
+              height="8px"
+              rounded="md"
+              mr={3}
+              color="green"
+            />
+          </Flex>
+          <MText type="text-md" pb={2}>
+            ETA: {secondsToDhms(epochRemainingInSec)}
+          </MText>
+        </Flex>
+        <Flex
+          bg={colors.white}
+          flexDirection="column"
+          justifyContent="space-between"
+          rounded="lg"
+          width="207px"
+          height="139px"
+          zIndex={5}
+          py={5}
+          pr={3}
+          pl={6}
+          mt={8}
+        >
+          <Flex justifyContent="space-between">
+            <MText type="text-md">{t("appPage.info-week-apy")}</MText>
+            <IconButton
+              variant="link"
+              aria-label="Info APY"
+              size="sm"
+              icon={<MdInfoOutline />}
+            />
+          </Flex>
+          <MText type="heading-xsm">{weekAPY}%</MText>
+          <MLink font="text-lg" color={colors.green} pb={2}>
+            {t("appPage.info-see-performance-action")}
+          </MLink>
+        </Flex>
+        <Flex
+          bg={colors.white}
+          flexDirection="column"
+          justifyContent="space-between"
+          rounded="lg"
+          width="207px"
+          height="139px"
+          zIndex={5}
+          py={5}
+          pr={3}
+          pl={6}
+          mt={8}
+        >
+          <Flex justifyContent="space-between">
+            <MText type="text-md">{t("appPage.info-validators")}</MText>
+            <IconButton
+              variant="link"
+              aria-label="Info Validators"
+              size="sm"
+              icon={<MdInfoOutline />}
+            />
+          </Flex>
+          <MText type="heading-xsm">{numberWithCommas(validators)}</MText>
+          <MLink font="text-lg" color={colors.green} pb={2}>
+            {t("appPage.info-validators-action")}
+          </MLink>
+        </Flex>
       </Flex>
       <Flex
+        display={["flex", "none"]}
         bg={colors.white}
         flexDirection="column"
         justifyContent="space-between"
         rounded="lg"
-        width="207px"
-        height="139px"
-        zIndex={5}
-        py={5}
-        px={6}
-        mt={8}
-      >
-        <MText type="text-md">{t("appPage.info-total-sol-staked")}</MText>
-        <MText type="heading-xsm">{numberWithCommas(totalSOLStaked)}</MText>
-        <MText type="text-md">
-          ≈ ${numberWithCommas(solPrice * totalSOLStaked)}
-        </MText>
-      </Flex>
-      <Flex
-        bg={colors.white}
-        flexDirection="column"
-        justifyContent="space-between"
-        rounded="lg"
-        width="207px"
-        height="139px"
+        width="95vw"
+        height="154px"
         zIndex={5}
         py={5}
         pr={3}
         pl={6}
-        mt={8}
+        m={2}
       >
-        <Flex justifyContent="space-between">
-          <MText type="text-md">{t("appPage.info-epoch")}</MText>
-          <IconButton
-            variant="link"
-            aria-label="Info epoch"
-            size="sm"
-            icon={<MdInfoOutline />}
-          />
+        <Flex justifyContent="space-between" pr={8}>
+          <MText type="text-md">{t("appPage.info-msol-sol-price")}</MText>
+          <MText type="heading-xsm">{mSOLvsSOLParity} SOL</MText>
         </Flex>
-        <Flex alignItems="center" justifyContent="space-between">
-          <MText type="heading-xsm">{epochProgress}%</MText>
+        <Flex justifyContent="space-between" pr={8}>
+          <MText type="text-md">{t("appPage.info-total-sol-staked")}</MText>
+          <MText type="heading-xsm">
+            {numberToShortVersion(totalSOLStaked)} SOL
+          </MText>
+        </Flex>
+        <Flex justifyContent="space-between" alignItems="center">
+          <MText type="text-md">{t("appPage.info-epoch")}</MText>
           <Progress
             value={epochProgress}
-            width="94px"
+            width="40vw"
             height="8px"
             rounded="md"
-            mr={3}
-            colorScheme="green"
+            color="green"
           />
+          <Flex>
+            <MText type="heading-xsm">{epochProgress}%</MText>
+            <IconButton
+              variant="link"
+              aria-label="Info epoch"
+              size="sm"
+              icon={<MdInfoOutline />}
+            />
+          </Flex>
         </Flex>
-        <MText type="text-md">ETA: {secondsToDhms(epochRemainingInSec)}</MText>
-      </Flex>
-      <Flex
-        bg={colors.white}
-        flexDirection="column"
-        justifyContent="space-between"
-        rounded="lg"
-        width="207px"
-        height="139px"
-        zIndex={5}
-        py={5}
-        pr={3}
-        pl={6}
-        mt={8}
-      >
-        <Flex justifyContent="space-between">
+        <Flex justifyContent="space-between" alignItems="center">
           <MText type="text-md">{t("appPage.info-week-apy")}</MText>
-          <IconButton
-            variant="link"
-            aria-label="Info APY"
-            size="sm"
-            icon={<MdInfoOutline />}
-          />
+          <Flex>
+            <MText type="heading-xsm">{weekAPY}%</MText>
+            <IconButton
+              variant="link"
+              aria-label="Info epoch"
+              size="sm"
+              icon={<MdInfoOutline />}
+            />
+          </Flex>
         </Flex>
-        <MText type="heading-xsm">{weekAPY}%</MText>
-        <MLink font="text-lg" colorScheme="green">
-          {t("appPage.info-see-performance-action")}
-        </MLink>
-      </Flex>
-      <Flex
-        bg={colors.white}
-        flexDirection="column"
-        justifyContent="space-between"
-        rounded="lg"
-        width="207px"
-        height="139px"
-        zIndex={5}
-        py={5}
-        pr={3}
-        pl={6}
-        mt={8}
-      >
-        <Flex justifyContent="space-between">
+        <Flex justifyContent="space-between" alignItems="center">
           <MText type="text-md">{t("appPage.info-validators")}</MText>
-          <IconButton
-            variant="link"
-            aria-label="Info Validators"
-            size="sm"
-            icon={<MdInfoOutline />}
-          />
+          <Flex>
+            <MText type="heading-xsm">{numberWithCommas(validators)}</MText>
+            <IconButton
+              variant="link"
+              aria-label="Info epoch"
+              size="sm"
+              icon={<MdInfoOutline />}
+            />
+          </Flex>
         </Flex>
-        <MText type="heading-xsm">{numberWithCommas(validators)}</MText>
-        <MLink font="text-lg" color={colors.green}>
-          {t("appPage.info-validators-action")}
-        </MLink>
       </Flex>
-    </Flex>
+    </>
   );
 };
 
