2431536f6fe22bd65c45c7d992f1505222f3b0bc
Alexandru.Stefan <alex@ndru.dev>
2021-11-07 00:17:44 +0100

    feat: adjust faq section for mobile res

--- src/components/app/FAQSection/index.tsx
+++ src/components/app/FAQSection/index.tsx
@@ -19,7 +19,6 @@ const FAQSection = () => {
   return (
     <Flex
       aria-label="faq-section"
-      px={{ base: 4, md: 0 }}
       pb={16}
       justifyContent="center"
       flexDirection="column"
@@ -27,57 +26,41 @@ const FAQSection = () => {
     >
       <Image
         src="/ilustrations/how-it-works1.svg"
-        width="428px"
-        height="257px"
+        width={["257px", "428px"]}
+        height={["154px", "257px"]}
         mb={4}
-        ml={-8}
+        ml={[0, -8]}
       />
-      <Flex width="480px" flexDirection="column">
-        <MHeading type="heading-sm" mt={8} mb={6}>
+      <Flex width={["288px", "480px"]} flexDirection="column">
+        <MHeading type="heading-sm" mt={8} mb={[5, 6]}>
           {t("appPage.faq-title")}
         </MHeading>
-        <Accordion mt={1}>
-          <AccordionItem bg={colors.white} rounded="md" mb={4}>
-            <AccordionButton pl={8} py={6} pr={6}>
-              <MText flex="1" type="text-xl" fontWeight="bold" textAlign="left">
-                {t("appPage.faq-first-q")}
-              </MText>
-              <AccordionIcon fontSize="2em" />
-            </AccordionButton>
-            <AccordionPanel pb={4} pl={8} pr={6}>
-              <MText type="text-md" lineHeight="170%">
-                {t("appPage.faq-first-response")}
-              </MText>
-            </AccordionPanel>
-          </AccordionItem>
-
-          <AccordionItem bg={colors.white} rounded="md" mb={4}>
-            <AccordionButton pl={8} py={6} pr={6}>
-              <MText flex="1" type="text-xl" fontWeight="bold" textAlign="left">
-                {t("appPage.faq-second-q")}
-              </MText>
-              <AccordionIcon fontSize="2em" />
-            </AccordionButton>
-            <AccordionPanel pb={8} pl={8} pr={6}>
-              <MText type="text-md" lineHeight="170%">
-                {t("appPage.faq-second-response")}
-              </MText>
-            </AccordionPanel>
-          </AccordionItem>
-
-          <AccordionItem bg={colors.white} rounded="md" mb={4}>
-            <AccordionButton pl={8} py={6} pr={6}>
-              <MText flex="1" type="text-xl" fontWeight="bold" textAlign="left">
-                {t("appPage.faq-third-q")}
-              </MText>
-              <AccordionIcon fontSize="2em" />
-            </AccordionButton>
-            <AccordionPanel pb={4} pl={8} pr={6}>
-              <MText type="text-md" lineHeight="170%">
-                {t("appPage.faq-third-response")}
-              </MText>
-            </AccordionPanel>
-          </AccordionItem>
+        <Accordion mt={[0, 1]}>
+          {[0, 1, 2].map((index) => (
+            <AccordionItem
+              key={`faq-item-${index}`}
+              bg={colors.white}
+              rounded="xl"
+              mb={4}
+            >
+              <AccordionButton pl={[4, 8]} py={[3, 6]} pr={[4, 6]}>
+                <MText
+                  flex="1"
+                  type="text-xl"
+                  fontWeight="bold"
+                  textAlign="left"
+                >
+                  {t(`appPage.faq-items.${index}.question`)}
+                </MText>
+                <AccordionIcon fontSize="2em" />
+              </AccordionButton>
+              <AccordionPanel pb={4} pl={[4, 8]} pr={[4, 6]}>
+                <MText type="text-md" lineHeight="170%">
+                  {t(`appPage.faq-items.${index}.response`)}
+                </MText>
+              </AccordionPanel>
+            </AccordionItem>
+          ))}
         </Accordion>
       </Flex>
     </Flex>
