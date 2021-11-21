68920c6b6f474f53f5a999c91464bf32d421a4d4
Alexandru.Stefan <alex@ndru.dev>
2021-11-21 13:22:52 +0100

    feat: bring more utils from old app

--- src/contexts/ConnectionProvider.tsx
+++ src/contexts/ConnectionProvider.tsx
@@ -2,7 +2,6 @@ import {
   Account,
   Commitment,
   Connection,
-  PublicKey,
   Transaction,
   TransactionInstruction,
 } from "@solana/web3.js";
@@ -11,75 +10,9 @@ import React, { FC, ReactNode, useContext, useEffect, useMemo } from "react";
 import TransactionLink from "../components/molecules/TransactionLink";
 import { useLocalStorageState } from "../hooks/useLocalStorageState";
 import { toastNotification } from "../utils/notification";
+import { DEFAULT_ENDPOINT, ENDPOINTS, ENV } from "../utils/web3/endpoints";
 
 import { WalletAdapter } from "./WalletAdapter";
-
-export type ENV = "mainnet-beta" | "testnet" | "devnet" | "localnet";
-
-export const ENDPOINTS = [
-  {
-    name: "devnet" as ENV,
-    endpoint: "https://api.devnet.solana.com/",
-    airdropEndpoint: "https://api.devnet.solana.com/",
-    maxAirdrop: 10,
-    keys: {
-      marinadeProgramId: new PublicKey(
-        "MarBmsSgKXdrN1egZf5sqe1TMai9K1rChYNDJgjq7aD"
-      ),
-      // Address of main state (instance)
-      marinadeStateId: new PublicKey(
-        "8szGkuLTAux9XMgZ2vtY39jVSowEcpBfFfD8hXSEqdGC"
-      ),
-    },
-  },
-  {
-    name: "mainnet-beta" as ENV,
-    // endpoint: 'https://solana-api.projectserum.com/',
-    endpoint: "https://marinade.rpcpool.com/",
-    airdropEndpoint: null,
-    keys: {
-      marinadeProgramId: new PublicKey(
-        "MarBmsSgKXdrN1egZf5sqe1TMai9K1rChYNDJgjq7aD"
-      ),
-      // Address of main state (instance)
-      marinadeStateId: new PublicKey(
-        "8szGkuLTAux9XMgZ2vtY39jVSowEcpBfFfD8hXSEqdGC"
-      ),
-    },
-  },
-  {
-    name: "testnet" as ENV,
-    endpoint: "https://api.testnet.solana.com",
-    airdropEndpoint: "https://api.testnet.solana.com",
-    maxAirdrop: 1,
-    keys: {
-      marinadeProgramId: new PublicKey(
-        "MarBmsSgKXdrN1egZf5sqe1TMai9K1rChYNDJgjq7aD"
-      ),
-      // Address of main state (instance)
-      marinadeStateId: new PublicKey(
-        "8szGkuLTAux9XMgZ2vtY39jVSowEcpBfFfD8hXSEqdGC"
-      ),
-    },
-  },
-  {
-    name: "localnet" as ENV,
-    endpoint: "http://127.0.0.1:8899",
-    airdropEndpoint: "http://127.0.0.1:8899",
-    keys: {
-      marinadeProgramId: new PublicKey(
-        "MarBmsSgKXdrN1egZf5sqe1TMai9K1rChYNDJgjq7aD"
-      ),
-      // Address of main state (instance)
-      marinadeStateId: new PublicKey(
-        "8szGkuLTAux9XMgZ2vtY39jVSowEcpBfFfD8hXSEqdGC"
-      ),
-    },
-  },
-];
-
-/// 0=devnet, 1=mainnet, 2=testnet
-export const DEFAULT_ENDPOINT = ENDPOINTS[0]; // TODO change to 1=mainnet for production
 
 interface ConnectionConfig {
   connection: Connection;
