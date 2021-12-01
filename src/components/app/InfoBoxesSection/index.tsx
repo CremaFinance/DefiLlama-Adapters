573bbd9b18a6b729b2d2502b1e5ca9eabcfa843b
phola <info@phola.com>
2021-12-01 12:54:39 +0000

    feat: stats context

--- src/components/app/InfoBoxesSection/index.tsx
+++ src/components/app/InfoBoxesSection/index.tsx
@@ -1,8 +1,14 @@
 import { Flex, Progress, Spinner } from "@chakra-ui/react";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
 import { useTranslation } from "next-export-i18n";
 
+import { useStats } from "../../../contexts/StatsContext";
 import { useEpochInfo } from "../../../hooks/useEpochInfo";
 import { usePrice } from "../../../hooks/usePrice";
+import {
+  format2Dec,
+  numberToShortVersion,
+} from "../../../utils/number-to-short-version";
 import MHeading from "../../atoms/Heading";
 import MLink from "../../atoms/Link";
 import MText from "../../atoms/Text";
@@ -10,7 +16,6 @@ import InfoIconWithTooltip from "components/molecules/InfoIconWithTooltip";
 import { coinSymbols } from "services/domain/coinSymbols";
 import colors from "styles/customTheme/colors";
 import { millisecondsToDhms } from "utils/ms-to-dmhs";
-import { numberToShortVersion } from "utils/number-to-short-version";
 
 const InfoBoxesSection = () => {
   const { t } = useTranslation();
@@ -18,9 +23,15 @@ const InfoBoxesSection = () => {
   const { data } = usePrice(coinSymbols.SOL);
   const epochData = useEpochInfo()?.data;
 
+  const { totalStaked } = useStats();
+
+  const totalSOLStaked = totalStaked
+    ? Number(format2Dec(totalStaked, LAMPORTS_PER_SOL))
+    : undefined;
+
   // TODO: Use actual values from services
   const mSOLvsSOLParity = 1.24;
-  const totalSOLStaked = 2345678;
+
   const weekAPY = 7.16;
   const validators = 411;
 
@@ -52,27 +63,33 @@ const InfoBoxesSection = () => {
             ≈ ${((data?.sol?.usd ?? 0) * mSOLvsSOLParity).toFixed(2)}
           </MText>
         </Flex>
-        <Flex
-          bg={colors.white}
-          flexDirection="column"
-          justifyContent="space-between"
-          rounded="lg"
-          width="207px"
-          height="139px"
-          zIndex={5}
-          py={5}
-          px={6}
-          mt={8}
-          mx={2}
-        >
-          <MText type="text-md">{t("appPage.info-total-sol-staked")}</MText>
-          <MHeading type="heading-xsm">
-            {totalSOLStaked.toLocaleString()}
-          </MHeading>
-          <MText type="text-md" pb={2}>
-            ≈ ${((data?.sol?.usd ?? 0) * totalSOLStaked).toLocaleString()}
-          </MText>
-        </Flex>
+        {totalSOLStaked ? (
+          <Flex
+            bg={colors.white}
+            flexDirection="column"
+            justifyContent="space-between"
+            rounded="lg"
+            width="207px"
+            height="139px"
+            zIndex={5}
+            py={5}
+            px={6}
+            mt={8}
+            mx={2}
+          >
+            <MText type="text-md">{t("appPage.info-total-sol-staked")}</MText>
+            <MHeading type="heading-xsm">
+              {totalSOLStaked.toLocaleString()}
+            </MHeading>
+            <MText type="text-md" pb={2}>
+              ≈ ${((data?.sol?.usd ?? 0) * totalSOLStaked).toLocaleString()}
+            </MText>
+          </Flex>
+        ) : (
+          <Flex flex={1} alignItems="center" justifyContent="center">
+            <Spinner size="md" mr={3} />
+          </Flex>
+        )}
         <Flex
           bg={colors.white}
           flexDirection="column"
@@ -190,12 +207,18 @@ const InfoBoxesSection = () => {
           <MText type="text-lg">{t("appPage.info-msol-sol-price")}</MText>
           <MHeading type="heading-2xsm">{mSOLvsSOLParity} SOL</MHeading>
         </Flex>
-        <Flex justifyContent="space-between" pr={8}>
-          <MText type="text-lg">{t("appPage.info-total-staked")}</MText>
-          <MHeading type="heading-2xsm">
-            {numberToShortVersion(totalSOLStaked)} SOL
-          </MHeading>
-        </Flex>
+        {totalSOLStaked ? (
+          <Flex justifyContent="space-between" pr={8}>
+            <MText type="text-lg">{t("appPage.info-total-staked")}</MText>
+            <MHeading type="heading-2xsm">
+              {numberToShortVersion(totalSOLStaked)} SOL
+            </MHeading>
+          </Flex>
+        ) : (
+          <Flex flex={1} alignItems="center" justifyContent="center">
+            <Spinner size="md" mr={3} />
+          </Flex>
+        )}
         <Flex justifyContent="space-between" alignItems="center">
           <MText type="text-lg">{t("appPage.info-epoch")}</MText>
           {epochData ? (
