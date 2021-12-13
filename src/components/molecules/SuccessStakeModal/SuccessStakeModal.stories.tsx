049291f3b8ffa4ba12e05adbbce4a763f6c8a8c1
Rijad Lendo <rijad.lendo@rubicon.ba>
2021-12-13 12:13:13 +0100

    feat: uptade successStakeModal strybook component

--- src/components/molecules/SuccessStakeModal/SuccessStakeModal.stories.tsx
+++ src/components/molecules/SuccessStakeModal/SuccessStakeModal.stories.tsx
@@ -1,7 +1,5 @@
 import { ComponentStory } from "@storybook/react";
 
-import Button from "../../atoms/Button";
-
 import SuccessStakeModal from "./index";
 
 export default { title: "Molecules/SuccessStakeModal" };
@@ -13,9 +11,6 @@ export const Default: ComponentStory<typeof SuccessStakeModal> = (args) => {
 Default.args = {
   stakedAmount: "12.15984",
   stakedCurrency: "mSOL",
-  children: ({ openModal }) => (
-    <Button variant="solid" onClick={openModal}>
-      Open modal
-    </Button>
-  ),
+  isOpen: true,
+  onClose: () => {},
 };
