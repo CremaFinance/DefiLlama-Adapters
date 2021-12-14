9406b3151e0c9db6f96d074961547b8ff4fd8392
Griffin McLennan <mclennangriffin@gmail.com>
2021-12-13 20:12:13 -0800

    feat: started retro card component

--- src/components/app/MndeSection/index.tsx
+++ src/components/app/MndeSection/index.tsx
@@ -1,5 +1,6 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Box } from "@chakra-ui/react";
 
+import MText from "../../atoms/Text";
 import colors from "styles/customTheme/colors";
 
 const MndeSection = () => {
@@ -14,7 +15,26 @@ const MndeSection = () => {
       flexDirection="column"
       backgroundColor={colors.greenLight}
       alignItems="stretch"
-    />
+    >
+      <Box
+        ml="auto"
+        mr="auto"
+        height={{ base: "466px", lg: "476px" }}
+        width={{ base: "288px", lg: "360px" }}
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        padding={{ base: "16px", lg: "32px" }}
+        background="white"
+        border="1px solid #E2E8F0"
+        box-boxSizing="border-box"
+        borderRadius="8px"
+      >
+        <Flex justifyContent="space-between">
+          <MText>MNDE Retro</MText>
+        </Flex>
+      </Box>
+    </Flex>
   );
 };
 
