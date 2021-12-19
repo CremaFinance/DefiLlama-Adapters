96a4f2715f9383ae13cbfa8eb42eff4d26b0ce8d
Alexandru Stefan <alex@ndru.dev>
2021-12-19 18:52:53 +0200

    feat: make card pixel perfect

--- src/components/molecules/MNDEFarmCard/index.tsx
+++ src/components/molecules/MNDEFarmCard/index.tsx
@@ -16,6 +16,7 @@ import { useQuarryProvider } from "contexts/QuaryContext";
 import { usePrices } from "hooks/usePrices";
 import { coinSymbols } from "services/domain/coinSymbols";
 import colors from "styles/customTheme/colors";
+import { addCommas } from "utils/add-commas";
 import { format2Dec, format5Dec } from "utils/number-to-short-version";
 
 const MNDEFarmCard = () => {
@@ -34,7 +35,7 @@ const MNDEFarmCard = () => {
   const mSOLvsSOLParity = marinadeState?.state?.st_sol_price
     ? marinadeState?.state?.st_sol_price?.toNumber() / 0x1_0000_0000
     : 0;
-  const solUSD = prices ? prices[coinSymbols.SOL]?.usd : 0;
+  const solUSD = prices[coinSymbols.SOL]?.usd;
   const mSolUSD = Number(format2Dec(solUSD ?? 0 * mSOLvsSOLParity));
   const totalDepositValue =
     (Math.round((totalDeposited?.toNumber() / LAMPORTS_PER_SOL) * 1e2) / 1e2) *
@@ -47,18 +48,18 @@ const MNDEFarmCard = () => {
       (totalDeposited?.toNumber() * mSolUSD);
   const apr =
     aprNum && !Number.isNaN(aprNum) ? Number(format2Dec(aprNum)) : undefined;
-  const userStake = mSOL?.minerData?.balance;
 
   const [timestamp, setTimestamp] = useState<BN>(
     new BN(Math.round(new Date().getTime() / 1000))
   );
 
+  const userStake = mSOL?.minerData?.balance || new BN(0);
   const rewardsPerTokenPaid = mSOL?.minerData?.rewardsPerTokenPaid || new BN(0);
   const rewardsEarned = mSOL?.minerData?.rewardsEarned || new BN(0);
 
   const rewards = mSOL?.quarry?.payroll?.calculateRewardsEarned(
     timestamp,
-    userStake ?? new BN(0),
+    userStake,
     rewardsPerTokenPaid,
     rewardsEarned
   );
@@ -78,99 +79,94 @@ const MNDEFarmCard = () => {
       width={{ base: "288px", lg: "360px" }}
       flexDirection="column"
       padding={{ base: "16px", lg: "32px" }}
+      mb={["16px", 0]}
       background="white"
       border="1px solid"
       borderColor={colors.lightGray}
       borderRadius="8px"
       justifyContent="space-between"
     >
-      {" "}
       <Box>
-        {" "}
-        <Flex width="100%" justifyContent="space-between" alignItems="center">
-          {" "}
-          <Flex alignItems="center">
-            {" "}
-            <Image src="/icons/mSOL.svg" boxSize="24px" mr="4px" />{" "}
-            <MText ml={1}>MSOL</MText>{" "}
-          </Flex>{" "}
-          <Image src="/icons/mnde.svg" boxSize="40px" />{" "}
-        </Flex>{" "}
-        <MHeading fontSize="22.5px" my="4px">
-          {" "}
-          {`${apr} % APR`}{" "}
-        </MHeading>{" "}
-        <MText>{`${format2Dec(
-          totalDeposited?.toNumber(),
-          LAMPORTS_PER_SOL
-        )} mSOL = $ ${format2Dec(totalDepositValue)} TVL`}</MText>{" "}
+        <Flex width="100%" justifyContent="space-between">
+          <Flex>
+            <Image src="/icons/mSOL.svg" boxSize="24px" mr="4px" />
+            <MText ml={1}>MSOL</MText>
+          </Flex>
+          <Image src="/icons/mnde.svg" boxSize="40px" />
+        </Flex>
+        <MHeading fontSize="22.5px" mb="4px">
+          {`${apr} % APR`}
+        </MHeading>
+        <MText type="text-md" mt="1px">{`${addCommas(
+          format2Dec(totalDeposited?.toNumber(), LAMPORTS_PER_SOL)
+        )} mSOL = $ ${addCommas(format2Dec(totalDepositValue))} TVL`}</MText>
         <Flex
           height="56px"
           width="100%"
-          marginY="12px"
+          mt="16px"
+          mb="16px"
           borderY="1px solid"
           borderColor={colors.lightGray}
           alignItems="center"
         >
-          {" "}
           <Icon
             as={IoCheckmarkCircle}
             color={colors.marinadeGreen}
-            width="20px"
-            height="20px"
+            width="24px"
+            height="24px"
             mr="10px"
-          />{" "}
-          <MText>{`${format2Dec(
-            mSOLFarmAnnualRewards * 7,
-            LAMPORTS_PER_SOL * 365
-          )} MNDE/week`}</MText>{" "}
-        </Flex>{" "}
+          />
+          <MText>{`${addCommas(
+            format2Dec(mSOLFarmAnnualRewards * 7, LAMPORTS_PER_SOL * 365)
+          )} MNDE/week`}</MText>
+        </Flex>
         <Flex
           justifyContent="space-between"
           display={connected ? "flex" : "none"}
         >
-          {" "}
-          <MText>{t("mndePage.your-deposit")}:</MText>{" "}
+          <MText>{t("mndePage.your-deposit")}:</MText>
           <MText>{`${format2Dec(
-            userStake ? userStake.toNumber() : 0,
+            userStake.toNumber(),
             LAMPORTS_PER_SOL
-          )} MSOL`}</MText>{" "}
-        </Flex>{" "}
+          )} MSOL`}</MText>
+        </Flex>
       </Box>
       {connected ? (
-        <Flex
-          height="60px"
-          width="100%"
-          marginTop="32px"
-          borderBottom="1px solid #EDF2F7"
-          borderTop="1px solid #EDF2F7"
-          alignItems="center"
-          justifyContent="space-between"
-        >
-          {" "}
-          <Flex alignItems="center">
-            {" "}
-            <Image src="/icons/mnde.svg" boxSize="24px" mr="4px" />{" "}
-            <MText>{`${format5Dec(
-              rewards?.toNumber(),
-              LAMPORTS_PER_SOL
-            )} MNDE`}</MText>{" "}
-          </Flex>{" "}
-          <MButton
-            variant="outline"
-            borderColor="gray"
-            color="black"
-            width={{ base: "70px", lg: "80px" }}
-            fontWeight="500"
-            fontSize="14.4px"
+        <Flex flexDirection="column" alignItems="center">
+          <MButton variant="solid" width="142px" height="40px">
+            {t("mndePage.manage-deposit-action")}
+          </MButton>
+          <Flex
+            height="60px"
+            width="100%"
+            marginTop="32px"
+            borderBottom="1px solid #EDF2F7"
+            borderTop="1px solid #EDF2F7"
+            alignItems="center"
+            justifyContent="space-between"
           >
-            {" "}
-            {t("mndePage.claim-action")}{" "}
-          </MButton>{" "}
+            <Flex alignItems="center">
+              <Image src="/icons/mnde.svg" boxSize="24px" mr="4px" />
+              <MText>{`${format5Dec(
+                rewards?.toNumber(),
+                LAMPORTS_PER_SOL
+              )} MNDE`}</MText>
+            </Flex>
+            <MButton
+              variant="outline"
+              borderColor="gray"
+              color="black"
+              width={{ base: "70px", lg: "80px" }}
+              fontWeight="500"
+              fontSize="14.4px"
+            >
+              {t("mndePage.claim-action")}
+            </MButton>
+          </Flex>
         </Flex>
       ) : (
         <Wallet />
-      )}{" "}
+      )}
     </Flex>
   );
 };
