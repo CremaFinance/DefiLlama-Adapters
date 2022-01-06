f433108348be7f065260e71dce48a6f6456ab96b
Lara <lara.madalinaa@gmail.com>
2022-01-07 00:39:42 +0200

    fix: possible NaN value

--- src/components/app/StakeInputsSection/BasicUnstake.tsx
+++ src/components/app/StakeInputsSection/BasicUnstake.tsx
@@ -95,7 +95,7 @@ const BasicUnstake = () => {
         maxUnstakeFee -
         ((maxUnstakeFee - minUnstakeFee) *
           ((liqPoolBalance || 0) - Number(stSolToUnstake) * LAMPORTS_PER_SOL)) /
-          state?.liq_pool?.lp_liquidity_target.toNumber();
+          (state?.liq_pool.lp_liquidity_target.toNumber() || 1);
     }
     return Math.min(3, Number(format2Dec(unstakefee)));
   };
