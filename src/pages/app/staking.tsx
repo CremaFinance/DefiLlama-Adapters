fd9d80bdeee5019510f2b1208e4fef441e15af79
phola <info@phola.com>
2022-01-11 12:25:14 +0000

    fix: remove extra provider

--- src/pages/app/staking.tsx
+++ src/pages/app/staking.tsx
@@ -1,6 +1,5 @@
 import { Box, Flex, Image } from "@chakra-ui/react";
 
-import { UserBalanceProvider } from "../../contexts/UserBalanceContext";
 import FAQSection from "components/app/FAQSection";
 import Footer from "components/app/Footer";
 import Header from "components/app/Header";
@@ -10,36 +9,34 @@ import colors from "styles/customTheme/colors";
 
 const Staking = () => {
   return (
-    <UserBalanceProvider>
-      <Box position="relative" overflow="hidden" bg={colors.greenLight}>
-        <Flex width="100vw" justifyContent="flex-end">
-          <Image
-            src="../../ilustrations/left-leaf.svg"
-            width={{ base: 0, md: "21vw", "2xl": "16vw" }}
-            alt="Leaf"
-            position="absolute"
-            right={-10}
-            top="10vh"
-            zIndex={3}
-          />
+    <Box position="relative" overflow="hidden" bg={colors.greenLight}>
+      <Flex width="100vw" justifyContent="flex-end">
+        <Image
+          src="../../ilustrations/left-leaf.svg"
+          width={{ base: 0, md: "21vw", "2xl": "16vw" }}
+          alt="Leaf"
+          position="absolute"
+          right={-10}
+          top="10vh"
+          zIndex={3}
+        />
 
-          <Image
-            src="../../ilustrations/right-leaf.svg"
-            width={{ base: 0, md: "16vw", "2xl": "16vw" }}
-            alt="Leaf"
-            position="absolute"
-            right={-14}
-            top="15vh"
-            zIndex={3}
-          />
-        </Flex>
-        <Header />
-        <StakingSection />
-        <StakeInputsSection />
-        <FAQSection />
-        <Footer />
-      </Box>
-    </UserBalanceProvider>
+        <Image
+          src="../../ilustrations/right-leaf.svg"
+          width={{ base: 0, md: "16vw", "2xl": "16vw" }}
+          alt="Leaf"
+          position="absolute"
+          right={-14}
+          top="15vh"
+          zIndex={3}
+        />
+      </Flex>
+      <Header />
+      <StakingSection />
+      <StakeInputsSection />
+      <FAQSection />
+      <Footer />
+    </Box>
   );
 };
 
