2dad7821ea96d69cd825e318ebca3b67b3f6ecf5
Griffin McLennan <mclennangriffin@gmail.com>
2022-01-18 10:37:25 -0800

    feat: add terms of use to footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -344,6 +344,36 @@ const Footer = () => {
               {t("indexPage.footer-roadmap")}
             </Link>
           </MButton>
+
+          <MButton
+            font="text-xl"
+            fontWeight={500}
+            variant="link"
+            color={colors.white}
+            bg="none"
+            _hover={{ textDecoration: "underline" }}
+            width="fit-content"
+            mb={2}
+            rightIcon={
+              <Icon
+                as={FiExternalLink}
+                width="16px"
+                height="16px"
+                cursor="pointer"
+              />
+            }
+          >
+            <Link
+              as={Link}
+              target="_blank"
+              href="https://marinade.finance/terms"
+              _hover={{ textDecoration: "none" }}
+              rel="noreferrer noopener"
+              _focus={{ boxShadow: "none" }}
+            >
+              {t("indexPage.footer-terms")}
+            </Link>
+          </MButton>
         </Box>
       </Box>
 
