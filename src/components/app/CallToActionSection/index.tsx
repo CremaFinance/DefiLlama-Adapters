a4091cf358896c06c6cdcd31b628f9edbbe4d30c
Alexandru Stefan <alex@ndru.dev>
2022-02-03 13:35:42 -0300

    fix: adjust responsiveness of the buttons

--- src/components/app/CallToActionSection/index.tsx
+++ src/components/app/CallToActionSection/index.tsx
@@ -7,84 +7,93 @@ const CallToActionSection = () => {
   const { t } = useTranslation();
 
   return (
-    <Flex
-      flexDirection={{ base: "column", lg: "row" }}
-      aria-label="call-to-action-section"
-      justifyContent={["center", "space-between"]}
-      alignContent="center"
-      mx={{ base: "14vw", md: "170px" }}
-      mt="12px"
-    >
-      <Button
-        variant="solid"
-        flex={[1, 0.33]}
-        height="48px"
-        fontSize="text-xl"
-        backgroundColor="transparent"
-        border="1px"
-        borderColor="black"
-        color="black"
-        _hover={{}}
-        rightIcon={<Image fill="black" src="/discord.svg" width="24px" />}
+    <Flex flex={1} justifyContent="center">
+      <Flex
+        flexDirection={{ base: "column", lg: "row" }}
+        flexWrap="wrap"
+        aria-label="call-to-action-section"
+        justifyContent="center"
+        alignContent="center"
+        minWidth={{ base: "288px", lg: "900px" }}
+        maxWidth={{ base: "320px", lg: "1100px" }}
+        mt="12px"
       >
-        <Link
-          as={Link}
-          target="_blank"
-          href="https://docs.marinade.finance/partnerships/list-on-marinades-defi-page"
-          _hover={{ textDecoration: "none" }}
-          rel="noreferrer noopener"
-          _focus={{ boxShadow: "none" }}
+        <Button
+          variant="solid"
+          width={["300px", "350px"]}
+          height="48px"
+          fontSize="text-xl"
+          backgroundColor="transparent"
+          border="1px"
+          borderColor="black"
+          color="black"
+          mx={5}
+          _hover={{}}
+          my={{ base: 0, lg: "8px" }}
+          rightIcon={<Image src="/discord-black.svg" width="24px" />}
         >
-          {t("appPage.cta-section.missing-integration")}
-        </Link>
-      </Button>
-      <Button
-        variant="solid"
-        flex={[1, 0.33]}
-        height="48px"
-        fontSize="text-xl"
-        backgroundColor="transparent"
-        border="1px"
-        borderColor="black"
-        color="black"
-        _hover={{}}
-        my={{ base: "24px", lg: 0 }}
-        rightIcon={<Image fill="black" src="/github.svg" width="24px" />}
-      >
-        <Link
-          as={Link}
-          target="_blank"
-          href="https://docs.marinade.finance/partnerships/list-on-marinades-defi-page"
-          _hover={{ textDecoration: "none" }}
-          rel="noreferrer noopener"
-          _focus={{ boxShadow: "none" }}
+          <Link
+            as={Link}
+            target="_blank"
+            href="https://docs.marinade.finance/partnerships/list-on-marinades-defi-page"
+            _hover={{ textDecoration: "none" }}
+            rel="noreferrer noopener"
+            _focus={{ boxShadow: "none" }}
+          >
+            {t("appPage.cta-section.missing-integration")}
+          </Link>
+        </Button>
+        <Button
+          variant="solid"
+          width={["300px", "350px"]}
+          height="48px"
+          fontSize="text-xl"
+          backgroundColor="transparent"
+          border="1px"
+          borderColor="black"
+          color="black"
+          mx={5}
+          _hover={{}}
+          my={{ base: "24px", lg: "8px" }}
+          rightIcon={<Image src="/github.svg" width="24px" />}
         >
-          {t("appPage.cta-section.list-your-integration")}
-        </Link>
-      </Button>
-      <Button
-        variant="solid"
-        flex={[1, 0.33]}
-        height="48px"
-        fontSize="text-xl"
-        backgroundColor="transparent"
-        border="1px"
-        borderColor="black"
-        color="black"
-        _hover={{}}
-        rightIcon={<Image fill="black" src="/discord.svg" width="24px" />}
-      >
-        <Link
-          as={Link}
-          target="_blank"
-          href="https://docs.marinade.finance/partnerships/list-on-marinades-defi-page"
-          _hover={{ textDecoration: "none" }}
-          rel="noreferrer noopener"
-          _focus={{ boxShadow: "none" }}
+          <Link
+            as={Link}
+            target="_blank"
+            href="https://docs.marinade.finance/partnerships/list-on-marinades-defi-page"
+            _hover={{ textDecoration: "none" }}
+            rel="noreferrer noopener"
+            _focus={{ boxShadow: "none" }}
+          >
+            {t("appPage.cta-section.list-your-integration")}
+          </Link>
+        </Button>
+        <Button
+          variant="solid"
+          width={["300px", "350px"]}
+          height="48px"
+          fontSize="text-xl"
+          backgroundColor="transparent"
+          border="1px"
+          borderColor="black"
+          color="black"
+          mx={5}
+          _hover={{}}
+          my={{ base: 0, lg: "8px" }}
+          rightIcon={<Image src="/sdk.svg" width="24px" />}
         >
-          {t("appPage.cta-section.integrate-with-marinade")}
-        </Link>
-      </Button>
+          <Link
+            as={Link}
+            target="_blank"
+            href="https://docs.marinade.finance/partnerships/list-on-marinades-defi-page"
+            _hover={{ textDecoration: "none" }}
+            rel="noreferrer noopener"
+            _focus={{ boxShadow: "none" }}
+          >
+            {t("appPage.cta-section.integrate-with-marinade")}
+          </Link>
+        </Button>
+      </Flex>
     </Flex>
   );
 };
