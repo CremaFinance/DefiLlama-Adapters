bbbed2252c6499383fed7e0fc891e0e929b2d0a2
rijad992 <rijad.lendo12@gmail.com>
2022-02-09 22:50:39 +0100

    fix: reuse why marinade component

--- src/components/app/WhySection/index.tsx
+++ src/components/app/WhySection/index.tsx
@@ -1,65 +1,45 @@
 import { Flex } from "@chakra-ui/layout";
-import { Image, Text, Box } from "@chakra-ui/react";
 import type { FunctionComponent } from "react";
 
+import MHeading from "../../atoms/Heading";
+import IconWithTextBelow from "components/molecules/IconWithTextBelow";
 import { useTranslation } from "hooks/useTranslation";
 import colors from "styles/customTheme/colors";
 
 const WhySection: FunctionComponent = () => {
   const { t } = useTranslation();
-  const cardIndexes = Array.from({ length: 3 }, (_, i) => i);
   return (
     <Flex
-      aria-label="why-section"
-      direction="column"
-      px={{ base: 4, md: "12vw" }}
-      py={20}
+      py={24}
       bg={colors.blackMate}
+      as="section"
+      aria-label="why-section"
+      flexDirection="column"
+      alignItems="stretch"
     >
-      <Text
-        color={colors.white800}
+      <MHeading
         textAlign="center"
-        fontWeight="bold"
-        fontSize={["30px", "43.95px"]}
-        pb={10}
+        color={colors.white}
+        type="heading-md"
+        mt={6}
       >
         {t("mndePage.why-section.header")}
-      </Text>
+      </MHeading>
       <Flex
-        flexDirection={["column", "column", "row"]}
-        flex={1}
-        justifyContent="space-between"
+        flexDirection={{ base: "column", md: "row" }}
+        justifyContent="center"
+        marginY={4}
       >
-        {cardIndexes.map((index) => {
-          return (
-            <Flex flexDirection="column" alignItems="center" mb={{ sm: 16 }}>
-              <Image
-                src={`/ilustrations/why-card-${index}.svg`}
-                width={["60px", "100px"]}
-                alt="Treasury"
-              />
-              <Text
-                color={colors.white800}
-                textAlign="center"
-                fontWeight="bold"
-                fontSize={["20px", "28px"]}
-                py={6}
-              >
-                {t(`mndePage.why-section.cards.${index}.header`)}
-              </Text>
-              <Box maxWidth="278px">
-                <Text
-                  color={colors.white800}
-                  textAlign="center"
-                  fontSize="18px"
-                  py={{ sm: 4 }}
-                >
-                  {t(`mndePage.why-section.cards.${index}.text`)}
-                </Text>
-              </Box>
-            </Flex>
-          );
-        })}
+        {[0, 1, 2].map((index) => (
+          <IconWithTextBelow
+            marginTop={8}
+            marginX={8}
+            key={`mnde-why-section-item-${index}`}
+            icon={`/ilustrations/why-card-${index}.svg`}
+            title={t(`mndePage.why-section.cards.${index}.header`) || ""}
+            subtitle={t(`mndePage.why-section.cards.${index}.text`)}
+          />
+        ))}
       </Flex>
     </Flex>
   );
