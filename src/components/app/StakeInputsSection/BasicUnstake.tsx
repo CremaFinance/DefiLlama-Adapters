136e4af69fd5dceeb76ca527319f2553d5bab395
Alexandru Stefan <alex@ndru.dev>
2022-02-24 22:28:54 +0100

    feat: add mathwallet option and reorder wallets

--- src/components/app/StakeInputsSection/BasicUnstake.tsx
+++ src/components/app/StakeInputsSection/BasicUnstake.tsx
@@ -375,6 +375,7 @@ const BasicUnstake = () => {
         tokenIcon="/icons/mSOL.svg"
         tokenBalance={stSOLBalance ?? 0}
         value={stSolToUnstake}
+        mb={2}
       />
       {walletConnected ? (
         <UnstakeOptions
