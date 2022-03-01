7a96b26981dae385a3eb1b601ed63f5c67a0597d
Rijad Lendo <39672277+rijad992@users.noreply.github.com>
2022-03-01 15:04:36 +0100

    Revert "fix: :zap: fix defi page performance issues"

--- src/hooks/usePools.tsx
+++ src/hooks/usePools.tsx
@@ -41,7 +41,6 @@ export const usePools = (providerOptions?: ProviderOptions) => {
       enabled: getProviderTokens(provider).every((t) => prices[t]),
       placeholderData: provider.pools,
       refetchInterval,
-      initialData: [],
     };
   });
 
