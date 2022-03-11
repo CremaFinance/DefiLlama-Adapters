e7be234a888a91d3dbd09bb73fe07131962c4e9c
Mihai Bodnariu <mihai.bodnariu@butonium.com>
2022-03-11 23:27:28 +0200

    fix: add action on relock button

--- src/components/molecules/NFTTable/index.tsx
+++ src/components/molecules/NFTTable/index.tsx
@@ -174,6 +174,7 @@ const NFTTable = () => {
       <ClaimMndeModal
         isPendingOpen={isPendingLockOpen}
         isOpen={isClaimMndeOpen}
+        onRelock={onCancelMndeOpen}
         onClaimConfirm={async (): Promise<boolean> => {
           setIsPendingLockOpen(true);
           return claimMNDE(currentNFTAddress).then(
