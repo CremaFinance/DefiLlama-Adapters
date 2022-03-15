f0ce61cc676c41647bed5aa735047112b06b37ac
Mihai Bodnariu <mihai.bodnariu@butonium.com>
2022-03-15 14:41:24 +0200

    fix: replace "tier" with "level"

--- src/components/molecules/NFTLevels/index.tsx
+++ src/components/molecules/NFTLevels/index.tsx
@@ -16,16 +16,16 @@ const NFTLevels = ({ balance, input, onLevelClick }: NFTLevelsProps) => {
   const placeholder = "{{amountLeft}}";
 
   const [selectedLevel, setSelectedLevel] = useState("none");
-  const [tierOneAmountLeft, setTierOneAmountLeft] = useState("0");
-  const [tierTwoAmountLeft, setTierTwoAmountLeft] = useState("0");
-  const [tierFourAmountLeft, setTierFourAmountLeft] = useState("0");
-  const [tierFiveAmountLeft, setTierFiveAmountLeft] = useState("0");
+  const [levelOneAmountLeft, setLevelOneAmountLeft] = useState("0");
+  const [levelTwoAmountLeft, setLevelTwoAmountLeft] = useState("0");
+  const [levelFourAmountLeft, setLevelFourAmountLeft] = useState("0");
+  const [levelFiveAmountLeft, setLevelFiveAmountLeft] = useState("0");
 
   useEffect(() => {
-    setTierOneAmountLeft("3,215");
-    setTierTwoAmountLeft("13");
-    setTierFourAmountLeft("213");
-    setTierFiveAmountLeft("3");
+    setLevelOneAmountLeft("3,215");
+    setLevelTwoAmountLeft("13");
+    setLevelFourAmountLeft("213");
+    setLevelFiveAmountLeft("3");
   }, []);
 
   useEffect(() => {
@@ -35,11 +35,11 @@ const NFTLevels = ({ balance, input, onLevelClick }: NFTLevelsProps) => {
       onLevelClick("-", false);
     }
     if (inputAmount > 999 && inputAmount < 5000 && inputAmount <= balance) {
-      setSelectedLevel("tier1");
+      setSelectedLevel("level1");
       onLevelClick("1", false);
     }
     if (inputAmount >= 5000 && inputAmount < 25000 && inputAmount <= balance) {
-      setSelectedLevel("tier2");
+      setSelectedLevel("level2");
       onLevelClick("2", false);
     }
     if (
@@ -47,7 +47,7 @@ const NFTLevels = ({ balance, input, onLevelClick }: NFTLevelsProps) => {
       inputAmount < 100000 &&
       inputAmount <= balance
     ) {
-      setSelectedLevel("tier3");
+      setSelectedLevel("level3");
       onLevelClick("3", false);
     }
     if (
@@ -55,11 +55,11 @@ const NFTLevels = ({ balance, input, onLevelClick }: NFTLevelsProps) => {
       inputAmount < 250000 &&
       inputAmount <= balance
     ) {
-      setSelectedLevel("tier4");
+      setSelectedLevel("level4");
       onLevelClick("4", false);
     }
     if (inputAmount >= 250000 && inputAmount <= balance) {
-      setSelectedLevel("tier5");
+      setSelectedLevel("level5");
       onLevelClick("5", false);
     }
   }, [balance, input, onLevelClick]);
@@ -86,78 +86,78 @@ const NFTLevels = ({ balance, input, onLevelClick }: NFTLevelsProps) => {
         zIndex={6}
       >
         <NFTLevelsItem
-          ilustration="/ilustrations/nft-tier1.svg"
+          ilustration="/ilustrations/nft-level1.svg"
           title={t("appPage.mnde.nft-levels.level-one.title")}
           amount={t("appPage.mnde.nft-levels.level-one.amount")?.replace(
             placeholder,
-            tierOneAmountLeft
+            levelOneAmountLeft
           )}
           onClick={() => {
-            setSelectedLevel("tier1");
+            setSelectedLevel("level1");
             if (balance >= 1000) onLevelClick("1", true);
           }}
-          selected={selectedLevel === "tier1"}
+          selected={selectedLevel === "level1"}
           disabled={balance < 1000}
           limited
         />
         <LevelDivider min={1000} max={5000} balance={balance} />
         <NFTLevelsItem
-          ilustration="/ilustrations/nft-tier2.svg"
+          ilustration="/ilustrations/nft-level2.svg"
           title={t("appPage.mnde.nft-levels.level-two.title")}
           amount={t("appPage.mnde.nft-levels.level-two.amount")?.replace(
             placeholder,
-            tierTwoAmountLeft
+            levelTwoAmountLeft
           )}
           onClick={() => {
-            setSelectedLevel("tier2");
+            setSelectedLevel("level2");
             if (balance >= 5000) onLevelClick("2", true);
           }}
-          selected={selectedLevel === "tier2"}
+          selected={selectedLevel === "level2"}
           disabled={balance < 5000}
           limited
         />
         <LevelDivider min={5000} max={25000} balance={balance} />
         <NFTLevelsItem
-          ilustration="/ilustrations/nft-tier3.svg"
+          ilustration="/ilustrations/nft-level3.svg"
           title={t("appPage.mnde.nft-levels.level-three.title")}
           amount={t("appPage.mnde.nft-levels.level-three.amount")}
           onClick={() => {
-            setSelectedLevel("tier3");
+            setSelectedLevel("level3");
             if (balance >= 25000) onLevelClick("3", true);
           }}
-          selected={selectedLevel === "tier3"}
+          selected={selectedLevel === "level3"}
           disabled={balance < 25000}
         />
         <LevelDivider min={25000} max={100000} balance={balance} />
         <NFTLevelsItem
-          ilustration="/ilustrations/nft-tier4.svg"
+          ilustration="/ilustrations/nft-level4.svg"
           title={t("appPage.mnde.nft-levels.level-four.title")}
           amount={t("appPage.mnde.nft-levels.level-four.amount")?.replace(
             placeholder,
-            tierFourAmountLeft
+            levelFourAmountLeft
           )}
           onClick={() => {
-            setSelectedLevel("tier4");
+            setSelectedLevel("level4");
             if (balance >= 100000) onLevelClick("4", true);
           }}
-          selected={selectedLevel === "tier4"}
+          selected={selectedLevel === "level4"}
           limited
           disabled={balance < 100000}
           mb="-9px"
         />
         <LevelDivider min={100000} max={250000} balance={balance} />
         <NFTLevelsItem
-          ilustration="/ilustrations/nft-tier5.svg"
+          ilustration="/ilustrations/nft-level5.svg"
           title={t("appPage.mnde.nft-levels.level-five.title")}
           amount={t("appPage.mnde.nft-levels.level-five.amount")?.replace(
             placeholder,
-            tierFiveAmountLeft
+            levelFiveAmountLeft
           )}
           onClick={() => {
-            setSelectedLevel("tier5");
+            setSelectedLevel("level5");
             if (balance >= 250000) onLevelClick("5", true);
           }}
-          selected={selectedLevel === "tier5"}
+          selected={selectedLevel === "level5"}
           limited
           disabled={balance < 250000}
         />
