3a797dc2e6551bea08fa6d175f2d14a3abdcc71e
Rijad Lendo <rijad.lendo@rubicon.ba>
2022-03-23 21:43:07 +0100

    fix: :lipstick: mobile notification

--- src/components/molecules/PerformanceNotification/index.tsx
+++ src/components/molecules/PerformanceNotification/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, IconButton } from "@chakra-ui/react";
+import { Box, Flex, IconButton, useMediaQuery } from "@chakra-ui/react";
 import { useTranslation } from "next-export-i18n";
 import type { FunctionComponent } from "react";
 import { useEffect, useState } from "react";
@@ -22,6 +22,7 @@ const PerformanceNotification: FunctionComponent<
   PerformanceNotificationProps
 > = ({ children, showNotification, onOpen, onClose }) => {
   const { t } = useTranslation();
+  const [isLargerThan320] = useMediaQuery("(min-width: 768px)");
   const [portalContainer, setPortalContainer] = useState<HTMLElement | null>(
     null
   );
@@ -34,7 +35,7 @@ const PerformanceNotification: FunctionComponent<
     { refetchInterval }
   );
   useEffect(() => {
-    if (!keepClosed && tps.data && tps.data < 1600) {
+    if (!keepClosed && tps.data && tps.data < 1600000000) {
       onOpen();
     }
 
@@ -55,31 +56,72 @@ const PerformanceNotification: FunctionComponent<
             position="relative"
             width="100%"
           >
-            <MdWarning color={colors.marinadeOrange} />
-            <Text
-              alignSelf="center"
-              ml="8px"
-              fontSize="14.4px"
-              py="8px"
-              fontWeight="medium"
-              lineHeight="20.16px"
-            >
-              {t("appPage.degraded-network-performance")}
-            </Text>
-            <IconButton
-              onClick={() => {
-                setKeepClosed(true);
-                onClose();
-              }}
-              position="absolute"
-              transform="translate(50%)"
-              right="20px"
-              variant="link"
-              aria-label="Close notification"
-              size="lg"
-              _focus={{ boxShadow: "none" }}
-              icon={<MdClose />}
-            />
+            {isLargerThan320 && (
+              <>
+                <MdWarning color={colors.marinadeOrange} />
+                <Text
+                  position="relative"
+                  alignSelf="center"
+                  textAlign="center"
+                  ml="8px"
+                  fontSize={{ base: "11px", md: "14px" }}
+                  py="8px"
+                  fontWeight="bold"
+                  lineHeight="20.16px"
+                  pr={{ base: "32px", md: "unset" }}
+                >
+                  {t("appPage.degraded-network-performance")}
+                </Text>
+                <IconButton
+                  onClick={() => {
+                    setKeepClosed(true);
+                    onClose();
+                  }}
+                  position="absolute"
+                  transform="translate(50%)"
+                  right="20px"
+                  variant="link"
+                  aria-label="Close notification"
+                  size="lg"
+                  _focus={{ boxShadow: "none" }}
+                  icon={<MdClose />}
+                />
+              </>
+            )}
+            {!isLargerThan320 && (
+              <Flex>
+                <Flex
+                  justifyContent="center"
+                  alignItems="center"
+                  py="8px"
+                  pl="1px"
+                >
+                  <Text textAlign="center" fontSize="11px" fontWeight="bold">
+                    <Box
+                      display="inline-block"
+                      pr="6px"
+                      transform="translate(0, 15%)"
+                    >
+                      <MdWarning color={colors.marinadeOrange} size={14} />
+                    </Box>
+                    {t("appPage.degraded-network-performance")}
+                  </Text>
+                </Flex>
+                <IconButton
+                  onClick={() => {
+                    setKeepClosed(true);
+                    onClose();
+                  }}
+                  transform="translate(50%)"
+                  right="20px"
+                  variant="link"
+                  aria-label="Close notification"
+                  size="lg"
+                  _focus={{ boxShadow: "none" }}
+                  icon={<MdClose />}
+                />
+              </Flex>
+            )}
           </Flex>
           {children}
         </Flex>,
