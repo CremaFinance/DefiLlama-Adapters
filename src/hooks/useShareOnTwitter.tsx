c3341ef0151c43d1aab74bd645fc86da3fa60d1f
Rijad Lendo <rijad.lendo@rubicon.ba>
2022-03-30 23:39:05 +0200

    feat: :sparkles: twitter share messages

--- src/hooks/useShareOnTwitter.tsx
+++ src/hooks/useShareOnTwitter.tsx
@@ -3,17 +3,36 @@ import { useTranslation } from "next-export-i18n";
 import { getNftImageForTwitter } from "services/marinade/getNftImageForTwitter";
 import { getRandomNumber } from "utils/get-random-number";
 
+import { useEditions } from "./useEditions";
+
 const useShareOnTwitter = () => {
   const { t } = useTranslation();
+  const { data } = useEditions() || [];
 
-  const messages = [t("mndePage.twitter-messages.message-one")];
+  const limitedMessages = [
+    t("mndePage.twitter-messages.limited.message-one"),
+    t("mndePage.twitter-messages.limited.message-two"),
+  ];
+
+  const regularMessages = [
+    t("mndePage.twitter-messages.regular.message-one"),
+    t("mndePage.twitter-messages.regular.message-two"),
+    t("mndePage.twitter-messages.regular.message-three"),
+  ];
 
   return async (index: number, tier: number) => {
+    const editionInfo = data?.find((x) => x !== undefined);
+    const isLimited = !!editionInfo?.left;
+    const messages: string[] = isLimited ? limitedMessages : regularMessages;
     const messageIndex = getRandomNumber(0, messages.length - 1);
     const twitterData = await getNftImageForTwitter(index, tier);
-
     window.open(
-      `https://twitter.com/intent/tweet?text=${messages[messageIndex]}&url=https://${twitterData.entities.media[0].display_url}`,
+      `https://twitter.com/intent/tweet?text=${messages[messageIndex].replace(
+        "{{left}}",
+        isLimited ? editionInfo?.left.toString() : ""
+      )}%0A%0Ahttps://marinade.finance/app/mnde/nft?pid=AVqM4cZzTfZvSNitUcgrRBQfGUAs4c66J3omrWFC2LWW&url=https://${
+        twitterData.entities.media[0].display_url
+      }`,
       "_blank"
     );
   };
